13587a57b54ed0b42db20f67c248c4b7590670dc
Szegedi Daniel <szegedi.dani22@gmail.com>
2018-04-14 18:04:19 +0200

    Adding build-up

---
+++ Frontend/hataridonaplo/src/app/main-page/main-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.pug',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
