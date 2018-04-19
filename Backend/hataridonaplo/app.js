a957ab21a66b667e54f7d9e02729c20508561d79
Szegedi Daniel <szegedi.dani22@gmail.com>
2018-04-19 11:02:16 +0200

    responsive pages, backend setup

--- Backend/hataridonaplo/app.js
+++ Backend/hataridonaplo/app.js
@@ -15,7 +15,9 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,12 +25,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,5 +39,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen('3500', () => {
+  console.log('Server is running');
+})
 
 module.exports = app;
--- Backend/hataridonaplo/routes/users.js
+++ Backend/hataridonaplo/routes/users.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const bodyparser = require('body-parser')
+
+const app = express();
+app.listen("4200", () => {
+  console.log("Server is running ata port 4200");
+})
+
+app.all("/register", (req, res) => {
+  console.log("access granted");
+  res.send(req.body)
+})
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
